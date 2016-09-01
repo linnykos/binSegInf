c17e5463d69eeabb85016d0eba83e8f568e83dfa
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-09-01 13:52:58 -0400

    Set up the simulation setup

--- main/phaseTransition-noJump.R
+++ main/phaseTransition-noJump.R
@@ -2,3 +2,45 @@ library(binSegInf)
 library(wbs)
 library(genlasso)
 
+simulationGenerator <- function(rule, paramMat, criterion, trials){
+  res <- lapply(1:nrow(paramMat), function(x){
+    sapply(1:trials, function(y){set.seed(y); criterion(rule(x))})
+  })
+  
+  names(res) <- sapply(1:nrow(paramMat), function(x){
+    paste0(paramMat[x,], collapse = "-")})
+  
+  res
+}
+
+.noJumpRuleClosure <- function(n, func){
+  function(void){func(CpVector(n, 0, NA)$data)}
+}
+
+.extractJumps <- function(obj, ...) UseMethod(".extractJumps")
+
+.extractJumpClosure <- function(numJumps){
+  function(res){.extractJumps(res, numJumps)}
+}
+
+.extractJumps.fusedlasso <- function(obj, numJumps, ...){
+  enumerateJumps(coef(obj, df = numJumps + 1)$beta)
+}
+
+.extractJumps.sbs <- function(obj, numJumps, ...){
+  order(obj$res[,"min.th"], decreasing = TRUE)[1:numJumps]
+}
+
+#####################################
+
+noJumpRuleFl <- .noJumpRuleClosure(100, fusedlasso1d)
+noJumpRuleBs <- .noJumpRuleClosure(100, sbs)
+noJumpCriterion <- .extractJumpClosure(1)
+paramMat <- matrix(0, 1, 1)
+trials <- 10
+
+resFl <- simulationGenerator(noJumpRuleFl, paramMat, noJumpCriterion, trials)
+resBs <- simulationGenerator(noJumpRuleBs, paramMat, noJumpCriterion, trials)
+
+
+
