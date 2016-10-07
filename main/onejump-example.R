dbaab0c5b4abbaf99911062582ed2ec374c46081
Sangwon Hyun <shyun@cmu.edu>
2016-10-06 21:51:19 -0400

    changed simulation code of basic one-jump example

--- main/onejump-example.R
+++ main/onejump-example.R
@@ -12,66 +12,82 @@ library(devtools)
 load_all()
 
 ## Source in the p-value functions from the selectiveInference package 
-# Replacement of all usages of pval.fl1d to poly.pval; 
 pval.fl1d <- function(y, G, dik, sigma, approx=T, threshold=T, approxtype = c("gsell","rob"), u = rep(0,nrow(G))){
   return(poly.pval(y, G, u, dik, sigma, bits=NULL)$pv)
 }
 
-## Setting 1
-## n = 4
-## set.seed(0)
-## y = c(rep(0,n/2),rep(5,n/2)) + rnorm(n,0,1)
-## thresh = .1
-
-## Setting 2
-## n = 60
-## sd = .5
-## mn = c(rep(-3, n/4), rep(2, n/4), rep(-1, n/4), rep(1, n/4))
-## set.seed(1)
-## y = mn + rnorm(n,0,sd)
-
-## Setting 3
-n = 20 
+n = 20
 sigma=1
 lev1=0
 lev2=3
 thresh = 2
 nsim=1000
+lambda=1
+lev2s = seq(from=0, to=5, by=.5)
 
+## Generate p-values
 pvals.list = list()
-pvals = matrix(NA,nrow=nsim,ncol=n)
-for(isim in 1:nsim){
-    cat('\r', isim, "out of", nsim)
-    ## Set it up
-    y = c(rep(lev1,n/2),rep(lev2,n/2)) + rnorm(n,0,sigma)
-    slist = elist = blist = Blist = zlist = Zlist =
-        matrix(NA, nrow = n, ncol = 2^8)
+for(ilev2 in 1:length(lev2s)){
+    cat(ilev2, "out of", length(lev2s), 'level2s', '\n')
+    pvals = matrix(NA, nrow=nsim, ncol=n)
+    lev2 = lev2s[ilev2]
+    for(isim in 1:nsim){
+        cat('\r', isim, "out of", nsim, 'simulations')
+        ## Set it up
+        y = c(rep(lev1,n/2),rep(lev2,n/2)) + rnorm(n,0,sigma)
+        slist = elist = blist = Blist = zlist = Zlist =
+            matrix(NA, nrow = n, ncol = 2^8)
 
-    ## Do Binary Segmentation
-    binseg(s = 1, e = n, j = 0, k = 1, thresh = thresh, y = y, n = n)
+        ## Do Binary Segmentation
+        tryCatch({
+            binseg(s = 1, e = n, j = 0, k = 1, thresh = lambda, y = y, n = n)}, error=function(e){})
+        
     
-    ## Bundle into one thing
-    bs.output = list(slist = slist, elist = elist, blist = blist, Blist = Blist,
-                     zlist = zlist, Zlist = Zlist, y = y, thresh = thresh)
+        ## Bundle into one thing
+        bs.output = list(slist = slist, elist = elist, blist = blist, Blist = Blist,
+                         zlist = zlist, Zlist = Zlist, y = y, thresh = lambda)
     
-    ## Get Gamma matrix and u vector
-    Gu = get.polyhedron(bs.output, verbose=F)
+        ## Get Gamma matrix and u vector
+        Gu = get.polyhedron(binseg.results = bs.output, lambda,verbose=F)
     
-    ## Conduct segment TG tests
-    test.b.list = sort(collapse(blist))
-    pvals[isim, test.b.list] = sapply(test.b.list,
-                                 function(test.b){return( pval.fl1d(y = y,
-                                                                    G = Gu$G,
-                                                                    dik = make.v(test.b, bs.output),
-                                                                    sigma = sigma,
-                                                                u = Gu$u))})
+        ## Conduct segment TG tests
+        test.b.list = sort(collapse(blist))
+        pvals[isim, test.b.list] = as.numeric(sapply(test.b.list,
+                                          function(test.b){return( pval.fl1d(y = y,
+                                                                             G = Gu$G,
+                                                                             dik = make.v(test.b, bs.output),
+                                                                             sigma = sigma,
+                                                                             u = Gu$u))}))
+    }
+    cat(fill=T)
+    pvals.list[[ilev2]] = pvals
 }
-pvals[test.b,]
-hist(pvals[,10])
-unif.sample = seq(from=0,to=1,length=nsim)
-qqplot(pvals[,10], unif.sample)
 
-replicate(100, do.once())
+sim.settings = list(lev1=lev1,lev2=lev2,thresh=thresh,nsim=nsim,sigma=sigma, n=n)
+save(pvals.list, sim.settings, file='onejump-by-lev2.Rdata')
 
-## If I want R to find stuff well, read this:
+## Plotting code
+
+
+## Plotting code
+load('onejump-by-lev2.Rdata')
+pdf("~/Desktop/onejump-by-lev2.pdf")
+par(mfrow=c(3,4))
+lev2 = 3
+set.seed(0)
+y = c(rep(lev1,n/2),rep(lev2,n/2)) + rnorm(n,0,sigma)
+plot(y, pch = 16, main = "data and signal (example)")
+lines(c(rep(lev1,n/2),rep(lev2,n/2)), col='red',lwd=2)
+for(ilev2 in 1:length(lev2s)){
+    hist(pvals.list[[ilev2]][,10],main="")
+    title(main=bquote(delta == .(lev2s[ilev2])))
+}
+graphics.off()
+
+## unif.sample = seq(from=0,to=1,length=nsim)
+## qqplot(pvals[,10], unif.sample)
+
+## replicate(100, do.once())
+
+## If I want R to use environments to find stuff, read this:
 ## http://blog.obeautifulcode.com/R/How-R-Searches-And-Finds-Stuff/
