40ca959e4a648a51dbaced47567af722b79a783d
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-11-10 22:23:53 -0500

    Added contrast spike and segments

--- binSegInf/R/contrast_vector.R
+++ binSegInf/R/contrast_vector.R
@@ -1,9 +1,23 @@
-contrast_vector.bsFs <- function(obj, jump.idx, sorted = F){
+contrast_vector.bsFs <- function(obj, jump.idx, sorted = F, 
+  type = c("segment", "spike")){
+  if(!is.character(type[1])) stop("type must be character")
+  if(!type[1] %in% c("segment", "spike")) stop("type must be either segment or spike")
+  type <- type[1]
+  
   jump.vec <- get_jumps(obj, sorted)
   jump <- jump.vec[jump.idx]
-  
   n <- .get_startEnd(obj$tree$name)[2]
+  
+  if(type == "segment") {
+    .contrast_vector_segment(obj, jump, n)
+  } else {
+    .contrast_vector_spike(obj, jump, n)
+  }
+}
+
+.contrast_vector_segment <- function(obj, jump, n){
   jumpSorted.vec <- c(1, get_jumps(obj, T), n)
+  
   idx <- which(jumpSorted.vec == jump)
   if(idx == 1) idx <- 2
   
@@ -11,6 +25,13 @@ contrast_vector.bsFs <- function(obj, jump.idx, sorted = F){
   res <- sign(.cusum_contrast_full(start, split, end, n))
   res[res>0] <- 1/sum(res > 0)
   res[res<0] <- -1/sum(res < 0)
+  
+  res
+}
+
+.contrast_vector_spike <- function(obj, jump, n){
+  res <- rep(0, n)
+  res[c(jump, jump+1)] <- c(-1, 1)
   
   res
 }
--- binSegInf/tests/testthat/test_contrast_vec.R
+++ binSegInf/tests/testthat/test_contrast_vec.R
@@ -12,3 +12,32 @@ test_that("contrast_vector.bsFs works", {
   expect_true(all(res[1:10] == -1/10))
   expect_true(all(res[11:15] == 1/5))
 })
+
+################################
+
+## .contrast_vector_segment is correct
+
+test_that(".contrast_vector_segment works", {
+  y <- c(rep(0, 10), rep(5, 5), rep(6, 5))
+  obj <- binSeg_fixedSteps(y, 2)
+  
+  res <- .contrast_vector_segment(obj, 10, 20)
+  
+  expect_true(all(res[1:10] == -1/10))
+  expect_true(all(res[11:15] == 1/5))
+})
+
+#############################################
+
+## .contrast_vector_spike is correct
+
+test_that(".contrast_vector_spike works", {
+  y <- c(rep(0, 10), rep(5, 5), rep(6, 5))
+  obj <- binSeg_fixedSteps(y, 2)
+  
+  res <- .contrast_vector_spike(obj, 10, 20)
+  
+  expect_true(all(res[c(10, 11)] == c(-1, 1)))
+  expect_true(all(res[1:9] == 0))
+  expect_true(all(res[12:20] == 0))
+})
