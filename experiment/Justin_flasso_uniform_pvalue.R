76de6c053311be0297a0e09acac374d274d3ee7f
klin1234@gmail.com <klin1234@gmail.com>
2017-01-19 21:24:26 -0500

    I realized justin and i don't use the same contrast. What??
    If the contrast is fixed, the both Justin and I don't get quite
    uniform p values.. So how did Justin make the contrast then?

--- experiment/Justin_flasso_uniform_pvalue.R
+++ experiment/Justin_flasso_uniform_pvalue.R
@@ -3,20 +3,36 @@ library(testthat)
 source("tests/testthat/test_flasso_vsGenLassoInf.R")
 
 pval_vec <- numeric(1000)
+pval_vec2 <- numeric(1000)
+
 for(i in 1:1000){
   set.seed(10*i)
-  #use justin's code
   n <- 100
   y <- rnorm(n, 0, 1)
+  
+  #use my code
+  obj <- fLasso_fixedSteps(y, 1)
+  poly <- polyhedra(obj, y)
+  contrast <- contrast_vector(obj, 1)
+  pval_vec2[i] <- pvalue(y, poly, contrast)
+  
+  #use justin's code
   D <- makeDmat(n, ord = 0)
   mypath <- dualpathSvd2(y, D, 1, approx = T)
   G <- getGammat.naive(obj = mypath, y = y, condition.step = 1)
-  d <- getdvec(mypath, y, 1, 1, type = "segment")
-  pval_vec[i] <- pval.fl1d(y, G$Gammat, d, 1)
+  #d <- getdvec(mypath, y, 1, 1, type = "segment")
+  pval_vec[i] <- pval.fl1d(y, G$Gammat, contrast, 1)
   
   if(i %% 100 == 0) cat('*')
 }
 
+par(mfrow = c(1,3))
 qqplot(pval_vec, seq(0, 1, length.out = length(pval_vec)), 
        pch = 16, xlab = "Actual quantile", ylab = "Theoretical quantile")
 lines(x = c(0,1), y = c(0, 1), col = "red", lwd = 2)
+
+qqplot(pval_vec2, seq(0, 1, length.out = length(pval_vec2)), 
+       pch = 16, xlab = "Actual quantile", ylab = "Theoretical quantile")
+lines(x = c(0,1), y = c(0, 1), col = "red", lwd = 2)
+
+plot(pval_vec, pval_vec2, xlab = "Justin", ylab = "Mine")
