6748be12b4e8b055f6f701a77755ed1b0b35796d
klin1234@gmail.com <klin1234@gmail.com>
2017-02-14 00:26:36 -0500

    Added tests to sample splitting

--- binSegInf/tests/testthat/test_sample_splitting.R
+++ binSegInf/tests/testthat/test_sample_splitting.R
@@ -23,3 +23,48 @@ test_that("sample_splitting works with fLasso_fixedSteps", {
   expect_true(res$method == "fLasso_fixedSteps")
   expect_true(res$jumps == 49)
 })
+
+###################################
+
+## jumps.ss is correct
+
+test_that("jumps.ss works", {
+  set.seed(10)
+  y <- c(rep(0, 50), rep(5, 50)) + 0.1*rnorm(100)
+  
+  obj <- sample_splitting(y, binSeg_fixedSteps, numSteps = 1)
+  res <- jumps(obj)
+  
+  expect_true(res == 49)
+})
+
+##################################
+
+## contrast_vector_ss is correct
+
+test_that("contrast_vector_ss works", {
+  set.seed(10)
+  y <- c(rep(0, 50), rep(5, 50)) + 0.1*rnorm(100)
+  
+  obj <- sample_splitting(y, binSeg_fixedSteps, numSteps = 1)
+  
+  res <- contrast_vector_ss(obj, 1)
+  expect_true(all(which(res < 0) == seq(2, 48, by = 2)))
+  expect_true(all(which(res > 0) == seq(52, 100, by = 2)))
+})
+
+#####################################
+
+## pvalue_ss is correct
+
+test_that("pvalue_ss works", {
+  set.seed(10)
+  y <- c(rep(0, 50), rep(5, 50)) + 0.1*rnorm(100)
+  
+  obj <- sample_splitting(y, binSeg_fixedSteps, numSteps = 1)
+  v <- contrast_vector_ss(obj, 1)
+  
+  res <- pvalue_ss(y, obj, v)
+  expect_true(length(res) == 1)
+  expect_true(res < 0.05)
+})
