5a7a8787bbd269cf421ae9bf38eb9bb9218dd2ad
Sangwon Hyun <shyun@cmu.edu>
2017-03-03 11:18:01 -0500

    Wrote functions to combine multiple polyhedra into a single polyhedron.

--- binSegInf/R/polyhedra.R
+++ binSegInf/R/polyhedra.R
@@ -1,3 +1,5 @@
+
+
 #' Generate polyhedra
 #'
 #' @param obj numeric matrix to be the gamma matrix
@@ -23,4 +25,24 @@ is_valid.polyhedra <- function(obj){
   if(nrow(obj$gamma) != length(obj$u)) stop("nrow(gamma) does not match length(u)")
   
   TRUE
+}
+
+#' Generic function to combine
+#' @param obj object
+combine <- function(obj, ...) {UseMethod("combine")}
+
+##' Combines several polyhedra to a single polyhedron
+##' @param ... polyhedra objects to add
+combine.polyhedra <- function(...){
+
+    polyobjs = list(...)
+    polyobjs = polyobjs[which(!sapply(polyobjs, is.null))]
+
+    ## Combine separately and return
+    newgamma = do.call(rbind, lapply(polyobjs, function(mypolyobj)mypolyobj$gamma))
+    newu = as.numeric(do.call(c, lapply(polyobjs, function(mypolyobj) mypolyobj$u)))
+
+    newpoly = polyhedra.matrix(obj = newgamma, u= newu)
+    stopifnot(is_valid.polyhedra(newpoly))
+    return(newpoly)
 }
