23f199bc135bee302a2f2b6ae4ae46899f6ec7ea
Sangwon Hyun <shyun@cmu.edu>
2017-04-26 12:17:51 -0400

    Adding an intermediate change to approximating the polyhedron.

--- binSegInf/R/wildBinSeg_fixedStep_polyhedra.R
+++ binSegInf/R/wildBinSeg_fixedStep_polyhedra.R
@@ -2,11 +2,12 @@
 #' Forms both Gamma matrix and u vector
 #'
 #' @param obj Output from wbs
+#' @param approx If TRUE, then does a Vup/Vlo comparison to see if you should add (chunks) of r
 #' @param ... not used now
 #'
 #' @return An object of class polyhedra
 #' @export
-polyhedra.wbsFs <- function(obj, ...){
+polyhedra.wbsFs <- function(obj, approx=FALSE,...){
 
     ## Basic checks
     stopifnot(is_valid.wbsFs(obj))
@@ -14,7 +15,7 @@ polyhedra.wbsFs <- function(obj, ...){
     ## Get all polyhedra
     actual.num.steps = (length(obj$B)-1)
     all.steps.polys <- lapply(1:actual.num.steps,
-                             function(mystep){ poly_from_snapshot(obj, mystep)})
+                             function(mystep){ poly_from_snapshot(obj, mystep, approx)})
 
     ## Combine polyhedron and return
     combined.poly = do.call(combine.polyhedra, all.steps.polys)
@@ -28,8 +29,7 @@ polyhedra.wbsFs <- function(obj, ...){
 ##' @param mystep step in the path
 ##'
 ##' @return a polyhedra object with the selection event at that step.
-poly_from_snapshot <- function(obj, mystep){
-  print(mystep)
+poly_from_snapshot <- function(obj, mystep,approx){
 
     ## Obtain snapshot
     Tcurr <- obj$T[[paste("step",mystep)]]
@@ -54,6 +54,7 @@ poly_from_snapshot <- function(obj, mystep){
     max.cusum.contrast = unsigned_contrast(max.s, max.b, max.e, y=obj$y)
     newrows1 = rBind(max.cusum.contrast)
 
+    if(!approx){
     ## For each terminal node, characterize the selection event of b.max in m.max.
     newpolylist <- lapply(Tcurr[!sapply(Tcurr,is.null)], function(t){
 
@@ -83,6 +84,43 @@ poly_from_snapshot <- function(obj, mystep){
             cat("nrow is", nrow(subtracted.contrasts),fill=TRUE)
             return(subtracted.contrasts)
         }))
+    }
+    if(!approx){
+
+      for(t in Tcurr[!sapply(Tcurr,is.null)]){
+        ## Get start/end points
+        s = extract(Scurr,t[1],t[2])
+        e = extract(Ecurr,t[1],t[2])
+        ms = which(.get_which_qualify(s,e,obj$intervals))
+        if(obj$augment) ms = c(ms,0)
+        if(length(ms)==0) return()
+
+        ## 2. Second, Compare /all other/ cusums to that of the grand max
+        newrows2 = do.call(rBind, lapply(ms, function(m){
+            if(m==0){se = c(s,e)}else{se = obj$intervals$se[[m]]}
+            s.to.e = (se[1]:se[2])
+            other.bs = s.to.e[-which(s.to.e == se[2])]
+            if(m==max.m) other.bs = other.bs[other.bs!=max.b]
+            if(length(other.bs)==0) return(rBind(rep(NA,length(obj$y)))[-1,])
+
+            ## Subtract all other contrast from the maximum cusum contrast
+            other.cusum.contrasts = do.call(rBind, lapply(other.bs, function(other.b){
+                signed_contrast(se[1], other.b, se[2], y=obj$y)}))
+            subtracted.contrasts = rBind(sweep(-rBind(other.cusum.contrasts), 2,
+                                               max.cusum.contrast, "+" ),
+                                         sweep(+rBind(other.cusum.contrasts), 2,
+                                               max.cusum.contrast, "+" ))
+            if(ncol(subtracted.contrasts)!=length(obj$y)) subtracted.contrasts = t(subtracted.contrasts)
+            cat("nrow is", nrow(subtracted.contrasts),fill=TRUE)
+            return(subtracted.contrasts)
+        }))
+        newrows.so.far = rBind(newrows.so.far, newrows)
+        ## Continue here -- compare vup and vlo and intersect if things have
+        ## changed. Actually, we need to check this across all steps,
+        ## /cumulatively/, which means the comparison has to happen /outside/ of
+        ## this function, in the main polyhedra.wbsfs() function..
+    }
+
         newrows = rBind(newrows1, newrows2)
         newu = rep(0,nrow(newrows))
 
