14e520403aaa2ddaac12bb72733ff663d4ba5910
klin1234@gmail.com <klin1234@gmail.com>
2017-08-09 03:41:22 -0400

    Added experiment to show binseg gives uniform pvalue

---
+++ experiment/pval_binseg.R
@@ -0,0 +1,28 @@
+library(binSegInf)
+## Simulation settings
+n <- 20
+numSteps <- 1
+n_levs <- 1
+lev <- 0
+nsim <- 3000
+sigma <- 1
+
+## Generates one/two-jumped means
+onejump <- function(lev,n){c(rep(0,n/2), rep(lev,n/2))}
+
+## Main loop
+pmat <- matrix(NA, ncol=2, nrow=nsim)
+colnames(pmat) <- c("cp", "pv")
+
+for(isim in 1:nsim){
+  y <- onejump(lev,n) + rnorm(n,0,sigma)
+  obj <- binSeg_fixedSteps(y, numSteps=numSteps)
+  poly <- polyhedra(obj)
+  contrast <- contrast_vector(obj, 1)
+  pmat[isim,"cp"] <- obj$cp
+  pmat[isim,"pv"] <- pvalue(y, poly, contrast)
+  
+  if(isim %% floor(nsim/10) == 0) cat('*')
+}
+
+plot(sort(pmat[,"pv"]), seq(0, 1, length.out = nsim))
