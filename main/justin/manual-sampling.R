36a78e773151c8fbf5dbdc90e79fb9fff52cd15f
Sangwon Hyun <shyun@cmu.edu>
2017-08-21 21:34:46 -0400

    manual sampling works, and gives uniform p-values to 1-step fused lasso and wbs

--- main/justin/manual-sampling.R
+++ main/justin/manual-sampling.R
@@ -1,27 +1,13 @@
-## Synopsis: See if manual sampling works.
-
-## Setting:
-n=10
+## Synopsis: See if manual sampling works. (vastly sampling original manual-sampling.R)
+n=20
 numIntervals= 10
 library(genlassoinf)
 library(binSegInf)
 library(MASS)
 
-
-## Helpers
-do.wildbinseg <- function(y){
-    I = binSegInf::generate_intervals(length(y), numIntervals)
-    original.model = binSegInf::wildBinSeg_fixedSteps(y,intervals=I, numSteps=1)
-}
-do.fl <- function(y){
-    original.model = genlassoinf::dualpathSvd2(y,
-                                               D=genlassoinf::makeDmat(length(y),ord=1,type='tf'),
-                                               maxsteps=1)
-}
-
 ## Try wild binary segmentation fused lasso (1 step)
 nsim = 1000
-nsamp = 200
+nsamp = 1000
 addsigma=.1
 ## method = "wbs"
 ## type="segment"
@@ -35,53 +21,61 @@ pvs = mclapply(1:nsim, function(isim){
     ## Original data / model / contrast
     mu = rep(0,n)
     y0 = mu + rnorm(n,0,1)
-    if(method=="wbs"){
-        original.model = do.wildbinseg(y0)
-    }
-    if(method=="fl"){
-        y1 = y0 + rnorm(n,0,addsigma)
-        original.model = do.fl(y0)
-    }
+
+    ## ## WBS
+    ## I0 = binSegInf::generate_intervals(length(y0), numIntervals)
+    ## original.model = wildBinSeg_fixedSteps(y0,intervals=I0, numSteps=1)
+    ## original.cp = original.model$cp * original.model$cp.sign
+
+    ## Fused lasso
+    noise0 = rnorm(n,0,0.1)
+    original.model = dualpathSvd2(y=y0 + noise0,
+                                  D=makeDmat(n,ord=1,type='tf'),
+                                  maxsteps=1)
     original.cp = original.model$cp * original.model$cp.sign
 
-    if(type=="fixed"){
-        v = runif(n)
-        v = v/sqrt(sum(v*v))
-    }
-    if(type=="segment"){
-        contrasts <- make_all_segment_contrasts(original.model)
-        v = contrasts[[as.character(original.cp[1])]]
-    }
+    contrasts <- make_all_segment_contrasts(original.model)
+    v = contrasts[[as.character(original.cp[1])]]
+    v = v/sqrt(sum(v*v))
+
+    ## v = runif(n)
+    ## v = v/sqrt(sum(v*v))
+
     Proj = cbind(v)%*%rbind(v)
     Proj.perp = diag(rep(1,n)) - cbind(v)%*%rbind(v)
 
     ## Generate new data
-    z0 = MASS::mvrnorm(nsamp, Proj%*%mu, Proj%*%t(Proj))
-    z1mat = apply(z0, 1, function(myrow) Proj.perp%*%y0 + myrow)
+    z0 = MASS::mvrnorm(nsamp, rep(0,n), Proj%*%t(Proj))
+    z1mat = apply(z0, 1, function(myrow) Proj.perp%*%(y0+noise0) + myrow) ## For Fused lasso
+    ## z1mat = apply(z0, 1, function(myrow) Proj.perp%*%(y0) + myrow) ## For WBS
     z1list = lapply(1:nsamp, function(isim)z1mat[,isim])
 
-    ## Rejection sample
-    if(method=="wbs"){
+    ## Sanity check
+    ## z00list = (lapply(z1list, function(z1) Proj.perp%*%z1))
+    ## stopifnot(all(sapply(z00list, all.equal, z00list[[1]])))
 
-        ## Sample intervals
-        Ilist = lapply(1:nsamp, function(isim)binSegInf::generate_intervals(n, numIntervals))
-        ## Get rejection sampled z's
-        cond.z1list = Map(function(z1,I){
-            new.model = binSegInf::wildBinSeg_fixedSteps(z1,intervals=I,numSteps=1)
-            new.cp = new.model$cp * new.model$cp.sign
-            if(all.equal(original.cp,new.cp)==TRUE) return(z1) else return(NULL)
-        },z1list,Ilist)
-    }
-    if(method=="fl"){
-        ## Get rejection sampled z's
-        cond.z1list = lapply(z1list, function(z1){
-            y1 = y0 + rnorm(n,0,addsigma)
-            new.cp = do.fl(y1)
-            if(all.equal(original.cp,new.cp)==TRUE) return(z1) else return(NULL)
-        })
-    }
+    ## Sample intervals (or noises)
+    Ilist = lapply(1:nsamp, function(isim)binSegInf::generate_intervals(n, numIntervals))
 
-    ## Form the quantile
+    ## ## Get WBS rejection sampled z's
+    ## cond.z1list = Map(function(z1,I){
+    ##     if(!any(I$starts <= abs(original.cp) & abs(original.cp)<I$ends)){
+    ##         return(NULL) ## This line causes some zeros?
+    ##     }
+    ##     new.model = wildBinSeg_fixedSteps(z1,intervals=I,numSteps=1)
+    ##     new.cp = new.model$cp * new.model$cp.sign
+    ##     if(all.equal(original.cp,new.cp)==TRUE) return(z1) else return(NULL)
+    ## },z1list,Ilist)
+
+    ## Get Fused Lasso rejection sampled z's
+    addednoiselist = lapply(1:nsim,function(isim){rnorm(n,0,0.1)})
+    cond.z1list = Map(function(z1,addednoise){
+        new.model = dualpathSvd2(z1+addednoise,maxsteps=1,D=makeDmat(n,ord=1,type="tf"))
+        new.cp = new.model$cp * new.model$cp.sign
+        if(all.equal(original.cp,new.cp)==TRUE) return(z1) else return(NULL)
+    },z1list,addednoiselist)
+
+    ## Form the p-value by taking the quantile
     cond.z1list = cond.z1list[which(!sapply(cond.z1list,is.null))]
     nsurvive = length(cond.z1list)
     a = lapply(cond.z1list, function(z1) sum(v*z1))
@@ -93,4 +87,7 @@ pvs = mclapply(1:nsim, function(isim){
 
 ## qqunif(unlist(pvs))
 
+## pdf("~/Desktop/example.pdf",width=5,height=5)
+## qqunif(c(unlist(pvs)))
+## graphics.off()
 
