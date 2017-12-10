c6f242d9d79980d48ca7124df5d3b0f60de14bdf
Sangwon Hyun <shyun@cmu.edu>
2017-12-10 11:17:22 -0500

    Fixing a mistake, where SBS plus was coded instead of FL plus

--- main/wbs-tests/sim-helpers.R
+++ main/wbs-tests/sim-helpers.R
@@ -282,6 +282,54 @@ dosim_compare <- function(type=c("wbs","fl.nonrand","fl.rand","sbs.rand",
                           locs=locs))
     }
 
+    if(type=="fl.rand.plus"){
+
+
+        ## Draw new noise
+        new.noise = rnorm(n,0,sigma.add)
+
+        ## 2. Get stopped FL p-values
+        numSteps = 10
+        g.fudged = dualpathSvd2(y + new.noise, maxsteps=numSteps, D = makeDmat(n,ord=0))
+        ic_obj = get_ic(g.fudged$cp, g.fudged$y, consec=consec, sigma=sigma, type="bic")
+        stoptime2 = ic_obj$stoptime
+
+        ## Get stopped polyhedron
+        if(ic_obj$flag=="normal" ){
+
+            ## Get model selection event polyhedron
+            Gobj.fudged = genlassoinf::getGammat.naive(obj=g.fudged, y=y, condition.step=stoptime2+consec)
+            poly.fudged = polyhedra(obj=Gobj.fudged$G, u=Gobj.fudged$u)
+
+            ## Get ic-stopped model selection polyhedron
+            ic_poly = ic_obj$poly
+
+            ## Combine them
+            combined.poly = polyhedra(obj = rbind(poly.fudged$gamma, ic_poly$gamma),
+                                      u = c(poly.fudged$u, ic_poly$u))
+
+            ## Postprocess and retain vicinity contrasts
+            cp = g.fudged$cp[1:stoptime1]
+            cp.sign = g.fudged$cp.sign[1:stoptime1]
+            cp = declutter_new(coords=cp, coords.sign=cp.sign, how.close=3)
+            cp.sign = sign(cp)
+            cp = abs(cp)
+            vlist <- make_all_segment_contrasts_from_cp(cp=cp, cp.sign=cp.sign, n=n)
+            retain = which(abs(as.numeric(names(vlist))) %in% visc)
+            vlist = vlist[retain]
+
+            ## Collect the p-values
+            pvs = sapply(vlist, function(v){
+                pv = randomize_addnoise(y=y, v=v, sigma=sigma, numIS=numIS,
+                                        sigma.add=sigma.add,
+                                        orig.fudged.poly= combined.poly)
+            })
+            locs = as.numeric(names(vlist))
+        } else {
+            return(data.frame(pvs=NA, locs=NA))
+        }
+    }
+
     if(type=="fl.nonrand"){
 
         ## Get nonrandomized p-value
@@ -306,6 +354,9 @@ dosim_compare <- function(type=c("wbs","fl.nonrand","fl.rand","sbs.rand",
         return(data.frame(pvs=pvs,
                           locs=locs))
     }
+
+
+
 
     if(type=="sbs.rand"){
 
@@ -335,49 +386,6 @@ dosim_compare <- function(type=c("wbs","fl.nonrand","fl.rand","sbs.rand",
                           locs=locs))
     }
 
-    if(type=="sbs.rand.plus"){
-
-        ## Draw new noise
-        new.noise = rnorm(n,0,sigma.add)
-
-        ## Get fudged SBS model
-        numSteps = 10
-        h.fudged = binSeg_fixedSteps(y + new.noise, numSteps=numSteps)
-        ic_obj = get_ic(h.fudged$cp, h.fudged$y, consec=consec, sigma=sigma, type="bic")
-        stoptime1 = ic_obj$stoptime
-
-        ## Get stopped polyhedron
-        if(ic_obj$flag=="normal" ){
-
-            ## Get model selection event polyhedron
-            poly.fudged = polyhedra(h.fudged, numSteps = stoptime1+consec)
-
-            ## Get ic-stoppage polyhedron
-            ic_poly = ic_obj$poly
-
-            ## Combine them
-            combined.poly = polyhedra(obj = rbind(poly.fudged$gamma, ic_poly$gamma),
-                                      u = c(poly.fudged$u, ic_poly$u))
-
-            ## Postprocess and retain vicinity contrasts
-            cp = h.fudged$cp[1:stoptime1]
-            cp.sign = h.fudged$cp.sign[1:stoptime1]
-            vlist <- make_all_segment_contrasts_from_cp(cp=cp, cp.sign=cp.sign, n=n)
-            numtests.before.retain.sbs <- length(vlist)
-            retain = which(abs(as.numeric(names(vlist))) %in% visc)
-            vlist = vlist[retain]
-
-            ## Collect the p-values
-            pvs = sapply(vlist, function(v){
-                pv = randomize_addnoise(y=y, v=v, sigma=sigma, numIS=numIS,
-                                        sigma.add=sigma.add,
-                                        orig.fudged.poly=combined.poly)
-            })
-            locs = as.numeric(names(vlist))
-        } else {
-            return(data.frame(pvs=NA, locs=NA))
-        }
-    }
 
     if(type=="sbs.nonrand"){
 
