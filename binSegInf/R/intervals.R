333cc97992dd945ce26471e5c21e91c90e1bff92
Sangwon Hyun <shyun@cmu.edu>
2018-03-08 01:44:06 -0500

    Improved intervals() to not make /all/ combinations first; this is prohibitive when n is large, like 1000

--- binSegInf/R/intervals.R
+++ binSegInf/R/intervals.R
@@ -17,50 +17,55 @@ intervals <- function(numIntervals, n, comprehensive=FALSE, existing=NULL, dista
                    msg = "|existing| must be a 2-column matrix with columns names s and e.")
     }
 
-    ## Make start-end candidates
-    starts = ends = c()
-    all.se = t(combn(n,2)) ## This is computationally intensive and quite unnecessary.
-    colnames(all.se) = c("s", "e")
-
-    ## Remove the pairs that are too close
-    too.close = apply(all.se, 1, function(myrow){
-        return((myrow["e"] - myrow["s"]) < distance)
-    })
-    if(any(too.close)){
-        all.se = all.se[-which(too.close),,drop=FALSE]
-    }
-
-    ## Remove the pairs that are too long
-    too.long = apply(all.se, 1, function(myrow){
-        return((myrow["e"] - myrow["s"]) > maxlength)
-    })
-    if(any(too.long)){
-        all.se = all.se[-which(too.long),,drop=FALSE]
-    }
-
-
-    ## If |existing| matrix is supplied, then exclude these from consideration.
-    if(!is.null(existing)){
-        to.exclude = apply(existing,1, function(myrow){
-            which((all.se[,1] == myrow["s"]) & (all.se[,2] == myrow["e"]))
-        })
-        all.se = all.se[-to.exclude,,drop=FALSE]
-    }
-
     ## If |comprehensive| == TRUE, draw /all/ possible intervals.
     if(comprehensive){
-        numIntervals = nrow(all.se)
-        random.i = 1:numIntervals
+        all.se = t(combn(n,2)) ## This is computationally intensive and quite unnecessary.
+        x.all = all.se[,1]
+        y.all = all.se[,2]
+        if(!is.null(existing))stop("Can't do both of: have comprehensive intervals, and exclude |existing| intervals!")
     } else {
-        ## Otherwise, draw random intervals, then store the starts and ends.
-        random.i = sample(nrow(all.se), numIntervals, replace=FALSE)
+        ## numIntervals = 10000
+        enough.intervals = FALSE
+        x.all = c()
+        y.all = c()
+        num.valid.intervals = 0
+        fac = 5 ## This is just a way to sample a lot initially
+        while(!enough.intervals){
+            x = sample(n, size=numIntervals*fac, replace=TRUE)
+            y = sample(n, size=numIntervals*fac, replace=TRUE)
+            too.close = which(unlist(Map(function(a,b){b-a<=distance}, x, y)))
+            too.long = which(unlist(Map(function(a,b){b-a>maxlength}, x, y)))
+            eliminate = c(too.close, too.long)
+            x.all = c(x.all, x[-eliminate])
+            y.all = c(y.all, y[-eliminate])
+            ## If |existing| matrix is supplied, then exclude these from consideration.
+            if(!is.null(existing)){
+                to.exclude = unlist(apply(existing,1, function(myrow){
+                    which((x.all == myrow["s"]) & (y.all == myrow["e"]))
+                }))
+                x.all = x.all[-to.exclude]
+                y.all = y.all[-to.exclude]
+            }
+            enough.intervals = (length(x.all) >= numIntervals)
+        }
     }
 
-    structure(list(starts=all.se[random.i, 1],
-                        ends=all.se[random.i, 2],
-                        cusummat=NA,
-                        numIntervals=numIntervals
-                        ), class="intervals")
+    ## Visualizing the frequency; should be even barplots!
+    ## xy = cbind(x.all, y.all)
+    ## plot((xy), xlim=c(0,n), ylim=c(0,n))
+    ## xylist = lapply(1:nrow(xy), function(irow)xy[irow,])
+    ## allpairs = unique(xylist)
+    ## freqs = rep(NA,length(allpairs))
+    ## for(ipair in 1:length(allpairs)){
+    ##     mypair = allpairs[[ipair]]
+    ##     freqs[ipair] = sum(sapply(xylist, function(myxy)all(myxy==mypair)))
+    ## }
+    ## barplot(freqs)
+
+    structure(list(starts=x.all,
+                   ends=y.all,
+                   cusummat=NA,
+                   numIntervals=numIntervals), class="intervals")
 }
 
 ##' Method to check if object is a valid object of the |interval| class.
