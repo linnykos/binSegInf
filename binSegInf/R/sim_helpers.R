1207cd883f0485bbae8b1f19118dd8734f26a54b
Sangwon Hyun <shyun@cmu.edu>
2018-03-25 18:05:38 -0400

    Added mean function

--- binSegInf/R/sim_helpers.R
+++ binSegInf/R/sim_helpers.R
@@ -4,7 +4,6 @@ onejump <- function(lev, n){
     c(rep(0,n/2),rep(lev,n/2))
 }
 
-
 ##' Functions to generate two-jump mean
 ##' @export
 twojump <- function(lev,n){c(rep(0,n/3),rep(lev,n/3), rep(0,n/3))}
@@ -13,3 +12,22 @@ twojump <- function(lev,n){c(rep(0,n/3),rep(lev,n/3), rep(0,n/3))}
 ##' Function to generate four-jump mean
 ##' @export
 fourjump <- function(lev,n){c(rep(0,n/5), rep(lev,n/5), rep(0,n/5), rep(-2*lev, n/5), rep(0,n/5) )}
+
+
+##' Function to generate four-jump mean with narrow spikes
+##' @export
+fourjump_spiky <- function(n, lev){
+    mn = rep(0, n)
+    mn[n/3+seq(from=-n/100, to = n/100)] = lev
+    mn[2*n/3+seq(from=-n/100, to = n/100)] = -2*lev
+    return(mn)
+}
+
+##' Function to generate four-jump mean with one spike and one plateau
+fourjump_hybrid <- function(n,lev){
+    mn = rep(0, n)
+    mn[n/3+seq(from=-n/100, to = n/100)] = lev
+    mn[2*n/3+seq(from=-n/10, to = n/10)] = -2*lev
+    return(mn)
+}
+
